495b6ebf2dd10eb6b85e22384cd8098c7e4df7ca
Escarleth Fernandez <escarleth.fernandez5@gmail.com>
2019-04-29 20:42:42 -0400

    updated function names

--- gulp-tasks-ef/dev/scripts/camera-capture.js
+++ gulp-tasks-ef/dev/scripts/camera-capture.js
@@ -1,12 +1,12 @@
 var video;
 
-function liveVideo() {
+function setup() {
 	createCanvas (320, 240);
 	background (51);
 	video = createCapture(VIDEO);
 	video.size(320, 240);
 }
 
-function drawImage() {
+function draw() {
 	image (video, 0, 0);
 }
